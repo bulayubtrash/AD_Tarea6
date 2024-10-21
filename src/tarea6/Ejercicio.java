9ac4d23d19a4bdc1dd3fe1d6f4ef07a7ee16e57c
bulayubtrash <bulayubtrash@gmail.com>
2024-10-21 08:52:15 +0200

    commit1

---
+++ src/tarea6/Ejercicio.java
@@ -0,0 +1,22 @@
+package tarea6;
+
+public class Ejercicio {
+	/**
+	 * Realiza un programa Java que trabajará con un fichero de alumnos, y muestra
+	 * un menú con el que poder realizar las siguientes operaciones: - Genera un
+	 * fichero vacío en el que se podrán añadir alumnos. A partir de esta opción,
+	 * este es el fichero que se usará. - Seleccionar un fichero: Permite
+	 * seleccionar un fichero existente que tenga alumnos para trabajar desde
+	 * entonces con él. - Cargar alumno: Lee del teclado los datos de un alumno, y
+	 * lo carga en el fichero en uso. - Mostrar todos los alumnos: Recorre el
+	 * fichero en uso y muestra todos los alumnos que tiene. Para cada alumno se
+	 * tiene la siguiente información: NIA (entero), Nombre (String), Apellidos
+	 * (String), Genero (Char), Fecha de Nacimiento (Fecha), Ciclo (String), Curso
+	 * (String), Grupo (String). Prueba el programa de la siguiente forma: - Crea un
+	 * fichero vacío. - Añade 2 alumnos. - Cierra el programa. - Vuelve a abrir el
+	 * programa. - Selecciona el fichero que has usado en los pasos anteriores. -
+	 * Añade 1 alumno. - Cierra el programa. - Vuelve a abrir el programa -
+	 * Selecciona el fichero que has usado en los pasos anteriores. - Muestra todos
+	 * los alumnos. - Cierra el programa.
+	 */
+}
